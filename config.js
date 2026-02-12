96cbfd0b05141bd91b8f773ade132912485a5ba2
whatupdl <daniel.lopez.buildit@gmail.com>
2026-02-12 01:38:51 -0600

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dldk2i8ws/video/upload/v1770881859/beabadoobee_-_Glue_Song_Lyric_Video_ft._Clairo_lzcsoa.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
